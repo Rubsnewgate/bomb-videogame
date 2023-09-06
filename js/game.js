77d36912dc184f1aa0f2e9b7fbea222387c7a6d1
Rubén Jaimes <sart.rubi98@gmail.com>
2023-09-06 15:50:39 -0600

    add(canvas): adding the responsive canvas and rendering the elements of the game

--- js/game.js
+++ js/game.js
@@ -1,8 +1,25 @@
 const canvas = document.querySelector('.game')
 const game = canvas.getContext('2d')
+let elementsSize
 
-window.addEventListener('load', startGame)
+window.addEventListener('load', setCanvasSize)
+window.addEventListener('resize', setCanvasSize)
+
+function setCanvasSize() {
+	const viewportSize = Math.min(window.innerWidth * .75, window.innerHeight * .75)
+	elementsSize = (viewportSize / 10) - 1
+
+	canvas.width = viewportSize
+	canvas.height = viewportSize
+	startGame()
+}
 
 function startGame() {
-	// funcion
+	game.font = elementsSize + 'px Verdana'
+
+	for (let i = 0; i < 10; i++) {
+		for (let z = 1; z < 11; z++) {
+			game.fillText(emojis['X'], elementsSize * i, elementsSize * z)
+		}
+	}
 }
