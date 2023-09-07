8b2a4d08b2ea6321355717734956d46c2ea148d8
Rubén Jaimes <sart.rubi98@gmail.com>
2023-09-07 11:06:59 -0600

    add(canvas): first version of the elements rendering

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector('.game')
 const game = canvas.getContext('2d')
+
 let elementsSize
 
 window.addEventListener('load', setCanvasSize)
@@ -7,19 +8,25 @@ window.addEventListener('resize', setCanvasSize)
 
 function setCanvasSize() {
 	const viewportSize = Math.min(window.innerWidth * .75, window.innerHeight * .75)
-	elementsSize = (viewportSize / 10) - 1
 
 	canvas.width = viewportSize
 	canvas.height = viewportSize
+
+	elementsSize = (viewportSize / 10) - 1
 	startGame()
 }
 
 function startGame() {
-	game.font = elementsSize + 'px Verdana'
+	game.font = `${elementsSize}px 'Montserrat'`
+	game.textAlign = 'end'
 
-	for (let i = 0; i < 10; i++) {
-		for (let z = 1; z < 11; z++) {
-			game.fillText(emojis['X'], elementsSize * i, elementsSize * z)
+	const map = maps[1]
+	const mapRows = map.trim().split('\n')
+	const mapColumns = mapRows.map(row => row.trim().split(''))
+
+	for (let row = 1; row <= 10; row++) {
+		for (let column = 1; column <= 10; column++) {
+			game.fillText(emojis[mapColumns[row - 1] [column - 1]], elementsSize * column, elementsSize * row)
 		}
 	}
 }
